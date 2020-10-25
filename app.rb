76a2bfe7818e12ea55ee26e705b996902b8e57ef
sergiocardenasg <sergiocardenas@utexas.edu>
2020-10-25 10:33:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @analyzed_phrase = PigLatinizer.new
+    
+        erb :result
+    end
 end
